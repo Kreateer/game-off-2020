ba51312249d2243fd5dedc26d4f1e8097b070967
Matija Milaković <kreateer@protonmail.com>
2020-11-04 22:16:33 +0100

    Create basic scenes (Related: $11e )
    This commit adds some of the basic scenes needed to create the prototype and updates the main Godot project file with new window settings

---
+++ scripts/Player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+signal hit
+export var speed = 400
+
+var screen_size # Size of the game window
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2() # Movement vector (has X and Y values)
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		#$AnimatedSprite.play()
+	else:
+		#$AnimatedSprite.stop()
+	
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.x = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Player_body_entered(body):
+	emit_signal("hit")
