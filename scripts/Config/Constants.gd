763f2d8b32743b7724203d110523f971707eabf1
Jonathan Nagy <nagytech@users.noreply.github.com>
2020-11-18 23:49:11 +1100

    Add Constants.gd

---
+++ scripts/Config/Constants.gd
@@ -0,0 +1,8 @@
+# Constant values spanning the entire game
+extends Node
+
+## Cause of death for the player
+const SUFFOCATE = "Suffocate"
+const DARKNESS = "Darkness"
+const HEALTH = "Health"
+const FALL = "Fall"
