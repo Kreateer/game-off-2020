22a5c00b67a89954a6c825c05042e5c915941b9b
Matija Milaković <kreateer@protonmail.com>
2020-11-24 20:20:40 +0100

    Attempt to fix Level conflicts and add levels to change in PopupScript
    This commit attempts to fix conflicts with the Level scenes and adds the Level scenes into BasePopupScript, so that the next level is loaded appropriately whenever the Player reaches the end point.

--- scripts/BasePopupScript.gd
+++ scripts/BasePopupScript.gd
@@ -1,18 +1,21 @@
 extends Popup
 
-onready var current_level = get_tree().get_current_scene().get_name()
+onready var tree = get_tree()
+onready var current_level = tree.get_current_scene().get_name()
 
 func _on_Continue_pressed():
 	# Check current scene and dictionary
 	# If current scene is in dictionary and is currently loaded
 	# Then load the next level.
-	print(current_level)
 	match current_level:
 		"Tutorial":
-			pass
-			#get_tree().change_scene("scenepathhere")
+			tree.change_scene("res://levels/Level1.tscn")
+		"Level1":
+			tree.change_scene("res://levels/Level2.tscn")
+		"Level3":
+			tree.change_scene("res://levels/Level3.tscn")
 	pass
 
 
 func _on_Exit_pressed():
-	get_tree().quit()
+	tree.quit()
